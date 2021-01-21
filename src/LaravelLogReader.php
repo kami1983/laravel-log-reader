087a3d735dea6050a71f59840f6097de305475ea
Kami1983 <meetcancanyou@yahoo.com>
2021-01-21 13:54:21 +0800

    First add mutil line reader .

--- src/LaravelLogReader.php
+++ src/LaravelLogReader.php
@@ -3,93 +3,117 @@
  * Creator: MD.HARUN-UR-RASHID
  * Date: 25/01/2020
  * Website: laravelarticle.com
+ * Modifier: Linhai
+ * Data: 20/01/2021
+ * Email:kami@cancanyou.com
  */
+
 namespace Haruncpi\LaravelLogReader;
 
 class LaravelLogReader
 {
-    protected $final = [];
-    protected $config = [];
+  protected $final = [];
+  protected $config = [];
 
 
-    public function __construct($config = [])
-    {
-        if (array_key_exists('date', $config)) {
-            $this->config['date'] = $config['date'];
-        } else {
-            $this->config['date'] = null;
-        }
+  public function __construct($config = [])
+  {
+    if (array_key_exists('date', $config)) {
+      $this->config['date'] = $config['date'];
+    } else {
+      $this->config['date'] = null;
+    }
 
+  }
+
+
+  public function getLogFileDates()
+  {
+    $dates = [];
+    $files = glob(storage_path('logs/laravel-*.log'));
+    $files = array_reverse($files);
+    foreach ($files as $path) {
+      $fileName = basename($path);
+      preg_match('/(?<=laravel-)(.*)(?=.log)/', $fileName, $dtMatch);
+      $date = $dtMatch[0];
+      array_push($dates, $date);
+    }
+
+    return $dates;
+  }
+
+  public function get()
+  {
+
+    $availableDates = $this->getLogFileDates();
+
+    if (count($availableDates) == 0) {
+      return response()->json([
+        'success' => false,
+        'message' => 'No log available'
+      ]);
+    }
+
+    $configDate = $this->config['date'];
+    if ($configDate == null) {
+      $configDate = $availableDates[0];
+    }
+
+    if (!in_array($configDate, $availableDates)) {
+      return response()->json([
+        'success' => false,
+        'message' => 'No log file found with selected date ' . $configDate
+      ]);
     }
 
 
-    public function getLogFileDates()
-    {
-        $dates = [];
-        $files = glob(storage_path('logs/laravel-*.log'));
-        $files = array_reverse($files);
-        foreach ($files as $path) {
-            $fileName = basename($path);
-            preg_match('/(?<=laravel-)(.*)(?=.log)/', $fileName, $dtMatch);
-            $date = $dtMatch[0];
-            array_push($dates, $date);
-        }
+    $pattern = "/^\[(?<date>.*)\]\s(?<env>\w+)\.(?<type>\w+):(?<message>.*)/m";
 
-        return $dates;
-    }
+    $fileName = 'laravel-' . $configDate . '.log';
+//    $content = file_get_contents(storage_path('logs/' . $fileName));
+//    preg_match_all($pattern, $content, $matches, PREG_SET_ORDER, 0);
 
-    public function get()
-    {
+    $logs = [];
+//    foreach ($matches as $match) {
+//      $logs[] = [
+//        'timestamp' => $match['date'],
+//        'env' => $match['env'],
+//        'type' => $match['type'],
+//        'message' => trim($match['message'])
+//      ];
+//    }
 
-        $availableDates = $this->getLogFileDates();
-
-        if (count($availableDates) == 0) {
-            return response()->json([
-                'success' => false,
-                'message' => 'No log available'
-            ]);
-        }
-
-        $configDate = $this->config['date'];
-        if ($configDate == null) {
-            $configDate = $availableDates[0];
-        }
-
-        if (!in_array($configDate, $availableDates)) {
-            return response()->json([
-                'success' => false,
-                'message' => 'No log file found with selected date ' . $configDate
-            ]);
-        }
-
-
-        $pattern = "/^\[(?<date>.*)\]\s(?<env>\w+)\.(?<type>\w+):(?<message>.*)/m";
-
-        $fileName = 'laravel-' . $configDate . '.log';
-        $content = file_get_contents(storage_path('logs/' . $fileName));
-        preg_match_all($pattern, $content, $matches, PREG_SET_ORDER, 0);
-
-        $logs = [];
-        foreach ($matches as $match) {
-            $logs[] = [
-                'timestamp' => $match['date'],
-                'env' => $match['env'],
-                'type' => $match['type'],
-                'message' => trim($match['message'])
-            ];
-        }
-
-        preg_match('/(?<=laravel-)(.*)(?=.log)/', $fileName, $dtMatch);
-        $date = $dtMatch[0];
-
-        $data = [
-            'available_log_dates' => $availableDates,
-            'date' => $date,
-            'filename' => $fileName,
-            'logs' => $logs
+    $file = fopen(storage_path('logs/' . $fileName),"r");   //打开文件
+    //检测指针是否到达文件的未端
+    while(! feof($file)) {
+      // 读取一行文本
+      $line_str = fgets($file);
+      if(preg_match($pattern, $line_str, $match)) {
+        $logs[] = [
+        'timestamp' => $match['date'],
+          'env' => $match['env'],
+          'type' => $match['type'],
+          'message' => trim($match['message'])
         ];
-
-        return response()->json(['success' => true, 'data' => $data]);
+      }else{
+        // 否则将剩余信息追加到文本消息的下方
+        $logs[count($logs)-1]['message'].= ("\n". $line_str);
+      }
     }
+    fclose($file);//关闭被打开的文件
+
+    preg_match('/(?<=laravel-)(.*)(?=.log)/', $fileName, $dtMatch);
+    $date = $dtMatch[0];
+
+    $data = [
+      'available_log_dates' => $availableDates,
+      'date' => $date,
+      'filename' => $fileName,
+      'logs' => $logs
+    ];
+
+    return response()->json(['success' => true, 'data' => $data]);
+  }
 
 }
+
